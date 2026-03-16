6cf4a98e2a6b7498b059db57eccf26f6e8e27d33
martinProg123 <225470683+martinProg123@users.noreply.github.com>
2026-03-16 17:57:56 +0800

    fix meta tag

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+// import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 const rootComponent = () => {
   return (
@@ -13,7 +13,7 @@ const rootComponent = () => {
       <main className="p-4">
         <Outlet /> {/* This is where your sub-pages render */}
       </main>
-      <TanStackRouterDevtools />
+      {/* <TanStackRouterDevtools /> */}
     </div>
   )
 }
